a8a370bba0f3b3c4c3baf5f54d7d3007d8bdc157
Michal Leskiewicz <leskiewicz02@gmail.com>
2022-06-17 18:15:54 +0200

    Rendering works properly now, remove button is not deleted on main page loading

--- src/functions/renderProject.js
+++ src/functions/renderProject.js
@@ -2,7 +2,8 @@ import {renderTodos} from "./renderTodo";
 import { createAddButton } from "./createAddButton";
 import { createRemoveProjectButton } from "./removeProjectButton";
 // import { renderProject } from "./renderProject";
-import { getProjects } from "./projectButtons";
+// import { getProjects } from "./projectButtons";
+import { loadMainPage } from "./mainPage";
 
 
 const renderProject = (todoList, selecProjectBtn) => {
@@ -13,11 +14,38 @@ const renderProject = (todoList, selecProjectBtn) => {
 
 const renderProjects = (projects) => {
 
+    // if (document.querySelector('#projectList').childNodes.length > 1) {
+    //     document.querySelector('#projectList').lastChild.remove();
+    // } else {
+    //     return
+    // }
+
+    document.querySelector('#projectList').innerHTML = '';
+
+    const homeButton = document.createElement('button');
+        homeButton.setAttribute('id', 'home');
+        homeButton.innerText = 'Home';
+        homeButton.addEventListener('click', loadMainPage);
+    
+        document.querySelector('#projectList').appendChild(homeButton);
+
+
     projects.forEach(project => {
         // const projectList = document.querySelector('#projectList');
         // for (let i = projectList.length; i > 1; i--) {
         //     projectList[i].remove();
         // }
+
+        // $('#projectList').children(':not(#home)').remove();
+
+        // if (document.querySelector('#projectList').childNodes.length > 1) {
+        //     document.querySelector('#projectList').lastChild.remove();
+        // } else {
+        //     return
+        // }
+
+        
+        
         
         const selectProject = document.createElement('button');
         selectProject.innerText = project.getProjectName();
@@ -26,6 +54,8 @@ const renderProjects = (projects) => {
             const todoList = project.getTodoList();
             renderProject(todoList, selecProjectBtn);
         }); 
+
+        // document.querySelector('#projectList').appendChild(homeButton);
         document.querySelector('#projectList').appendChild(selectProject)
     });
 }
